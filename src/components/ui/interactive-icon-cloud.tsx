7fbc45887f9f3582b5e52f5bceb0db6ec4655440
Paul Hwang <hwpaul1007@gmail.com>
2026-01-11 14:29:23 -0800

    finetune spinning control

--- src/components/ui/interactive-icon-cloud.tsx
+++ src/components/ui/interactive-icon-cloud.tsx
@@ -32,7 +32,7 @@ export const cloudProps: Omit<ICloud, "children"> = {
     tooltipDelay: 0,
     outlineColour: "#0000",
     maxSpeed: 0.04,
-    minSpeed: 0.02,
+    minSpeed: 0,
     // dragControl: false,
   },
 }
